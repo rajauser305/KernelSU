bff4b0a6b2a7136cacaaf56fb4e81429d41f076b
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-09-22 10:38:46 +0800

    kernel: kp_ksud: restore kprobes for early-boot and used-once hooks
    since kprobes offer dynamic hooking and shit, this is going to be better
    on something that we only need temporarily.
    
    this still keeps whole sucompat onto manual hooks as those are performance
    sensitive, needed to be permanent and "timeable".
    
    as for these hooks that got hooked here they are only used either only at
    boot or on some, used only once.
    
    symbols hooked:
    
    vfs_read
    - needed only at boot for read proxy-ing atrace.rc
    
    input_event
    - needed only up to like boot_complete, for 3-button-press safemode feature
    --
    
    as for unregistration, we defer this once boot is completed and then have a kthread
    unregister everything.
    
    credits: some of these are just straight up copied from upstream.
    
    Tests:
    ximi mi a2 lite, arm64, Linux 4.9
    samsung galaxy s3, arm, Linux 3.0
    https://github.com/backslashxx/KernelSU/issues/26
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- kernel/ksu.c
+++ kernel/ksu.c
@@ -14,6 +14,10 @@
 #include "ksud.h"
 #include "supercalls.h"
 
+#ifdef CONFIG_KSU_KPROBES_KSUD
+extern void kp_ksud_init();
+#endif
+
 extern void ksu_supercalls_init();
 
 int __init kernelsu_init(void)
@@ -39,6 +43,10 @@ int __init kernelsu_init(void)
 	ksu_allowlist_init();
 
 	ksu_throne_tracker_init();
+
+#ifdef CONFIG_KSU_KPROBES_KSUD
+	kp_ksud_init();
+#endif
 
 	return 0;
 }
