67bad8d1000de24756201d08c342f05c2aa48cb4
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-11-09 09:58:37 +0800

    kernel: restore code required for old kernels
    This commit restores compatibility code needed for legacy that was removed upstream.
    
    Restores LSM hooks:
    - inode_rename - replacss pkg_observer.c
    - task_fix_setuid - replaces setuid_hook.c + kernel_umount.c
    
    Restores workqueue'd allowlist handling dropped at bfb767b, 9060e61
    Restores -Wno-implicit-function-declaration dropped at d3a09fa
    Restores old core_hook infrastructure. breaking it up to setuid + kernel_umount is just more complexity here.
    Restores an ifdef on arch.h, adds in-32-bit arm support.
    Restores Makefile instead of Kbuild - TODO: review this later for UL
    Restore needed rules for init's execution of ksud - removed on 6e918fc, pr 3031
    
    smaller changes and cleanups
    	sucompat: ksu_handle_stat(), remove dead ifdef.
    	LINUX_VERSION_CODE / KERNEL_VERSION, ksu.c
    		- reported by Sinclair19
    		- fix by including version.h
    	fatal_signal_pending, ksud.c
    		- add compat by including sched.h or sched/signal.h conditionally
    		- ref: https://github.com/torvalds/linux/commit/2a1f062a4acf0be50516ceece92a7182a173d55a
    	selinux_state.ss, core_hook.c
    		- remove rcu_dereference use
    		- ref: tiann#2695
    	seccomp.filter_count, core_hook.c
    		- reset this only for 5.9 and up as it only exists there
    		- ref: tiann#2708, https://github.com/gregkh/linux/commit/c818c03b661cd769e035e41673d5543ba2ebda64
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- kernel/app_profile.c
+++ kernel/app_profile.c
@@ -1,7 +1,13 @@
+#include <linux/version.h>
 #include <linux/capability.h>
 #include <linux/cred.h>
 #include <linux/sched.h>
-#include <linux/sched/signal.h>
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 10, 0)
+#include <linux/sched/signal.h> // signal_struct
+#include <linux/sched/task.h>
+#else
+#include <linux/sched.h>
+#endif
 #include <linux/seccomp.h>
 #include <linux/thread_info.h>
 #include <linux/uidgid.h>
@@ -13,6 +19,7 @@
 #include "selinux/selinux.h"
 #include "su_mount_ns.h"
 #include "sucompat.h"
+#include "kernel_compat.h"
 
 #if LINUX_VERSION_CODE >= KERNEL_VERSION (6, 7, 0)
 static struct group_info root_groups = { .usage = REFCOUNT_INIT(2) };
@@ -52,7 +59,11 @@ static void setup_groups(struct root_profile *profile, struct cred *cred)
 			put_group_info(group_info);
 			return;
 		}
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 9, 0)
 		group_info->gid[i] = kgid;
+#else
+		GROUP_AT(group_info, i) = kgid;
+#endif
 	}
 
 	groups_sort(group_info);
@@ -76,9 +87,14 @@ static void disable_seccomp()
 	memcpy(&fake, current, sizeof(fake));
 	current->seccomp.mode = 0;
 	current->seccomp.filter = NULL;
+
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 9, 0)
 	atomic_set(&current->seccomp.filter_count, 0);
+#endif
+
 	spin_unlock_irq(&current->sighand->siglock);
 
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 9, 0)
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 11, 0)
 	// https://github.com/torvalds/linux/commit/bfafe5efa9754ebc991750da0bcca2a6694f3ed3#diff-45eb79a57536d8eccfc1436932f093eb5c0b60d9361c39edb46581ad313e8987R576-R577
 	fake.flags |= PF_EXITING;
@@ -86,8 +102,8 @@ static void disable_seccomp()
 	// https://github.com/torvalds/linux/commit/0d8315dddd2899f519fe1ca3d4d5cdaf44ea421e#diff-45eb79a57536d8eccfc1436932f093eb5c0b60d9361c39edb46581ad313e8987R556-R558
 	fake.sighand = NULL;
 #endif
-
 	seccomp_filter_release(&fake);
+#endif // 5.9
 }
 
 void escape_with_root_profile(void)
